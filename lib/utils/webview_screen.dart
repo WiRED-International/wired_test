2e246b6bc5f039430268b4ed70e1553a0e158d23
Sean Bristol-Lee <seanbristol81@gmail.com>
2024-09-05 01:01:23 -0700

    view pdf files from module

--- lib/utils/webview_screen.dart
+++ lib/utils/webview_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:open_filex/open_filex.dart';
 
 class WebViewScreen extends StatelessWidget {
   final URLRequest urlRequest;
@@ -31,12 +36,33 @@ class WebViewScreen extends StatelessWidget {
             allowsInlineMediaPlayback: true,
           ),
         ),
-        // onDownloadStart: (controller, url) async {
-        //   if (url != null) {
-        //     _downloadFile(url);
-        //   }
-        // }
+        onDownloadStartRequest: (controller, downloadStartRequest) async {
+          if (downloadStartRequest.mimeType == "application/pdf") {
+            print("Download URL: ${downloadStartRequest.url}");
+            // Check for local file existence
+            await _openLocalPdf(downloadStartRequest.url.toString());
+          }
+        }
       ),
     );
+  }
+
+  Future<void> _openLocalPdf(String url) async {
+    try {
+      // Decode the URL if needed
+      String filePath = Uri.parse(url).path;
+      File file = File(filePath);
+
+      print("Checking for file at: $filePath");
+
+      if (await file.exists()) {
+        print("Opening PDF file at: $filePath");
+        await OpenFilex.open(filePath);
+      } else {
+        print("PDF file does not exist: $filePath");
+      }
+    } catch (e) {
+      print("Error opening PDF file: $e");
+    }
   }
 }
