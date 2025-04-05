f2483f879c806b61921b7b6b47db3dea46cc062e
Sean Bristol-Lee <seanbristol81@gmail.com>
2025-04-05 00:18:40 -0700

    add to menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wired_test/pages/home_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:wired_test/providers/auth_provider.dart';
@@ -28,19 +29,51 @@ Future<void> main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale? _selectedLocale;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  void _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final code = prefs.getString('locale');
+    if (code != null) {
+      setState(() {
+        _selectedLocale = Locale(code);
+      });
+    }
+  }
+
+  void _changeLocale(Locale locale) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('locale', locale.languageCode);
+    setState(() {
+      _selectedLocale = locale;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      supportedLocales: [
-        const Locale('en', ''), // English
-        const Locale('zh', ''), // Mandarin (Chinese)
-        // Add other locales as needed
+      locale: _selectedLocale,
+      supportedLocales: const [
+        Locale('en'),
+        Locale('zh'),
+        Locale('es'),
       ],
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
@@ -50,14 +83,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'WiRED International'),
+      home: MyHomePage(
+        title: 'WiRED International',
+        onLocaleChange: _changeLocale,
+      ),
       debugShowCheckedModeBanner: false,
-      // initialRoute: '/',
-      // routes: {
-      //   '/': (context) => const MyHomePage(title: 'WiRED International'), // Open to all users
-      //   '/restricted': (context) =>  AuthGuard(child: RestrictedPage()), // Restricted page
-      //   '/open': (context) => const OpenPage(), // Open to all users
-      // },
     );
   }
 }
--- lib/pages/menu/guestMenu.dart
+++ lib/pages/menu/guestMenu.dart
@@ -16,6 +16,10 @@ import 'meet_team.dart';
 
 
 class GuestMenu extends StatefulWidget {
+  final void Function(Locale)? onLocaleChange;
+
+  const GuestMenu({Key? key, this.onLocaleChange}) : super(key: key);
+
   @override
   _GuestMenuState createState() => _GuestMenuState();
 }
@@ -189,18 +193,25 @@ class _GuestMenuState extends State<GuestMenu> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    _buildInkWellButton('Meet The Team', scalingFactor, () {
-                      print("Meet The Team Tapped");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MeetTeam()),
-                      );
+                    _buildInkWellButton('Meet The Team', scalingFactor, () async {
+                      final Uri url = Uri.parse('https://sites.google.com/view/wired-international-team/home');
+                      if (await canLaunchUrl(url)) {
+                        await launchUrl(url, mode: LaunchMode.externalApplication);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Could not launch Meet The Team')),
+                        );
+                      }
                     }),
-                    _buildInkWellButton('About WiRED', scalingFactor, () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AboutWired()),
-                      );
+                    _buildInkWellButton('About WiRED', scalingFactor, () async {
+                      final Uri url = Uri.parse('https://sites.google.com/view/healthmap-about/home');
+                      if (await canLaunchUrl(url)) {
+                        await launchUrl(url, mode: LaunchMode.externalApplication);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Could not launch About WiRED')),
+                        );
+                      }
                     }),
                   ],
                 ),
@@ -413,18 +424,25 @@ class _GuestMenuState extends State<GuestMenu> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      _buildInkWellButton('Meet The Team', scalingFactor, () {
-                        print("Meet The Team Tapped");
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => MeetTeam()),
-                        );
+                      _buildInkWellButton('Meet The Team', scalingFactor, () async {
+                        final Uri url = Uri.parse('https://sites.google.com/view/wired-international-team/home');
+                        if (await canLaunchUrl(url)) {
+                          await launchUrl(url, mode: LaunchMode.externalApplication);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Could not launch Meet The Team')),
+                          );
+                        }
                       }),
-                      _buildInkWellButton('About WiRED', scalingFactor, () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AboutWired()),
-                        );
+                      _buildInkWellButton('About WiRED', scalingFactor, () async {
+                        final Uri url = Uri.parse('https://sites.google.com/view/healthmap-about/home');
+                        if (await canLaunchUrl(url)) {
+                          await launchUrl(url, mode: LaunchMode.externalApplication);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Could not launch About WiRED')),
+                          );
+                        }
                       }),
                     ],
                   ),
